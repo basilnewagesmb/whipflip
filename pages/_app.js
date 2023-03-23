e3fd2751b940389254659572aa82627de77068ae
Basil Babu <basilb@newagesmb.com>
2023-03-23 16:22:25 +0530

    react-ga

--- pages/_app.js
+++ pages/_app.js
@@ -74,4 +74,7 @@ function MyApp({ Component, pageProps, analytics, fbpixel, hotjar }) {
     </Provider>
   );
 }
-export default MyApp;
+export default withPixel(
+  2810107665901141,
+  Router
+)(withHotjar(2096064, 6, Router)(withGA("UA-173303436-1", Router)(MyApp)));
