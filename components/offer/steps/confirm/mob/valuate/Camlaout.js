306556b6d6a3660f2e6cd556ab94266ffa13d656
Basil Babu <basilb@newagesmb.com>
2023-06-02 19:03:25 +0530

    test

--- components/offer/steps/confirm/mob/valuate/Camlaout.js
+++ components/offer/steps/confirm/mob/valuate/Camlaout.js
@@ -90,6 +90,7 @@ function CamLayout({
         }}
         steps={steps}
       />
+      <button onClick={() => setOpen(true)}>setOpen(false);</button>
       <Layout className="vh-100 overflow-hidden">
         <Sider
           collapsedWidth={60}
