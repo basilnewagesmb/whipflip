ac0e03b7a80f67ea7e1936d39d5fe583c5098814
Basil Babu <basilb@newagesmb.com>
2023-08-02 18:06:49 +0530

    caps revert

--- middleware.js
+++ middleware.js
@@ -7,10 +7,4 @@ export function middleware(request) {
   if (hasError) {
     return NextResponse.redirect(new URL("/", request.url));
   }
-  if (request.nextUrl.pathname === request.nextUrl.pathname.toLowerCase())
-    return NextResponse.next();
-
-  return NextResponse.redirect(
-    new URL(request.nextUrl.origin + request.nextUrl.pathname.toLowerCase())
-  );
 }
