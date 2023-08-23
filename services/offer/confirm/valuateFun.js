9f27df2af71a3d2247c48963ddf45c2f81a3e772
Basil Babu <basilb@newagesmb.com>
2023-08-23 12:49:54 +0530

    access_token

--- services/offer/confirm/valuateFun.js
+++ services/offer/confirm/valuateFun.js
@@ -223,7 +223,7 @@ function useValuateFun({ offerData, analytics, fbpixel, isForUpload }) {
         }
         const loginRes = await pegasusLogin();
         if (loginRes?.data?.access_token) {
-          pegasusUpload({ data: bodyFormData, token: loginRes.data.token })
+          pegasusUpload({ data: bodyFormData, token: loginRes?.data?.access_token })
             .then(async (result) => {
               if (!!result?.data?.Result?.QuoteId) {
                 let dData = result.data;
